0d6ad40e86726bc89dd7b539159507481c013cb9
Viviana <vivicalvat007@gmail.com>
2022-01-04 15:11:02 -0500

    adding error with errors variant

--- api/src/middlewares/errorHandler.js
+++ api/src/middlewares/errorHandler.js
@@ -1,4 +1,6 @@
 module.exports = (err, req, res, next) => {
+   err.errors && res.status(400).json(err.errors);
+
    const status = err.status || 500;
    const message = err.message || 'Server Error';
    console.error(err);
